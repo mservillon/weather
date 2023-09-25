a292985cfbf21483ccff1e2ab0783e80a8f37285
mservillon <mackservillon@gmail.com>
2023-09-25 17:20:46 -0500

    Setting up weather input....

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,6 +3,17 @@ function Weather() {
     return (
         <div className="weather">
             <span className="title">What's the Weather today?</span>
+            <form action="">
+                <label for="city">Enter city: </label>
+                <input type="text" name="city"  />
+                <br />
+
+                <label for="country">Enter country: </label>
+                <input type="text" name="country"  />
+                <br />
+                
+                <button className="getWeather">Get Weather</button>
+            </form>
         </div>
     )
 }
