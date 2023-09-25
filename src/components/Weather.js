bc19f9ee469cb5da04a2363fac2b347403aeaed8
mservillon <mackservillon@gmail.com>
2023-09-25 17:06:24 -0500

    Connected Weather.js to App.js
    Created a new file called "Weather.js" and stored it in the components folder then connected it to App.js

---
+++ src/components/Weather.js
@@ -0,0 +1,10 @@
+
+function Weather() {
+    return (
+        <div className="weather">
+            <span className="title">What's the Weather today?</span>
+        </div>
+    )
+}
+
+export default Weather;
